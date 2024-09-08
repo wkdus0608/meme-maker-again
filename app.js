4d73ad1cbdad75a4dbdb961b4db2786b92ab1be4
wkdus0608 <seongmin020608@gmail.com>
2024-09-08 15:55:25 +0900

    선 굵기 변경 기능

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const line_Width = document.getElementById("line-width");
 
 canvas.width = 700;
 canvas.height = 700;
+ctx.lineWidth = 5;
 
 isPainting = false;
 
-function line(event) {
+function drawing(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY)
         ctx.stroke();
@@ -19,9 +21,14 @@ function stopDrawing() {
     isPainting = false;
     ctx.beginPath();
 }
+function lineWidthChange(event) {
+    console.log(event.target.value);
+    ctx.lineWidth = event.target.value;
+}
 
 
-canvas.addEventListener("mousemove", line);
+canvas.addEventListener("mousemove", drawing);
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mouseup", stopDrawing);
 canvas.addEventListener("mouseleave", stopDrawing);
+line_Width.addEventListener("change", lineWidthChange);
