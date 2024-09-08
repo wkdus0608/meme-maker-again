8166773b21952133a26ea30164ac97b4de343152
wkdus0608 <seongmin020608@gmail.com>
2024-09-08 16:27:26 +0900

    Fill, eraser, clear mode

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const line_Width = document.getElementById("line-width");
+const color = document.getElementById("color");
+const colorOptions = Array.from(document.getElementsByClassName("color-options"));
+const fillBtn = document.getElementById("fillBtn");
+const eraserBtn = document.getElementById("eraserBtn");
+const cleanAll = document.getElementById("cleanAll");
 
 canvas.width = 700;
 canvas.height = 700;
 ctx.lineWidth = 5;
 
 isPainting = false;
+isFilling = false;
 
 function drawing(event) {
     if (isPainting) {
@@ -22,8 +28,36 @@ function stopDrawing() {
     ctx.beginPath();
 }
 function lineWidthChange(event) {
-    console.log(event.target.value);
     ctx.lineWidth = event.target.value;
+}
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+}
+function onColorOptionsChange(event) {
+    ctx.strokeStyle = event.target.dataset.color;
+    ctx.fillStyle = event.target.dataset.color;
+    color.value = event.target.dataset.color;
+}
+function fillModeClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+function fillMode() {
+    if (!isFilling) {
+        isFilling = true;
+        fillBtn.innerText = "Draw";
+    } else {
+        isFilling = false;
+        fillBtn.innerText = "Fill";
+    }
+}
+function eraserMode() {
+    isFilling = false;
+    ctx.strokeStyle = "white";
+}
+function clear() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 
@@ -32,3 +66,9 @@ canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mouseup", stopDrawing);
 canvas.addEventListener("mouseleave", stopDrawing);
 line_Width.addEventListener("change", lineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach(color => color.addEventListener("click", onColorOptionsChange));
+canvas.addEventListener("click", fillModeClick);
+fillBtn.addEventListener("click", fillMode);
+eraserBtn.addEventListener("click", eraserMode);
+cleanAll.addEventListener("click", clear);
